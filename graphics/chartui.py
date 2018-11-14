e01baecaa613d3f40fbecae413c9c052897779e0
duoyi-window <1323242382@qq.com>
2018-11-14 14:44:40 +0800

    优化

--- graphics/chartui.py
+++ graphics/chartui.py
@@ -2,7 +2,7 @@
 import weakref
 from . import slotui
 from ui.BlueChartWidget import Ui_BlueChartWidget
-from PyQt5.QtWidgets import QGraphicsProxyWidget, QGraphicsTextItem, QWidget
+from PyQt5.QtWidgets import QGraphicsProxyWidget, QGraphicsTextItem, QGraphicsItem, QWidget
 from PyQt5.QtCore import Qt, QPointF
 from PyQt5.QtGui import QFont
 
@@ -14,14 +14,17 @@ class CBlueChartUI(QGraphicsProxyWidget):
         self.m_Scene = weakref.ref(oScene)
         self.m_StartPos = None
         self.m_ChartIsMoving = False
-
-        sName = oBlueChart.GetName()
         self.m_BlueChartWidget = Ui_BlueChartWidget()
         self.m_BlueChartWidgetParent = slotui.CWidget()
+        self.m_ChangeCharName = None
+        self.InitUI()
+        self.InitSingle()
+
+    def InitUI(self):
         self.m_BlueChartWidget.setupUi(self.m_BlueChartWidgetParent)
         self.m_BlueChartWidgetParent.GetStyle()
+        sName = self.m_BlueChart().GetName()
         self.m_BlueChartWidget.lb_Title.setText(sName)
-
         self.m_ChangeCharName = QGraphicsTextItem(self.m_BlueChartWidget.lb_Title.text(), self)
         self.m_ChangeCharName.setTextInteractionFlags(Qt.TextEditorInteraction)
         self.m_ChangeCharName.setCursor(Qt.IBeamCursor)
@@ -30,11 +33,24 @@ class CBlueChartUI(QGraphicsProxyWidget):
         self.m_ChangeCharName.setFont(font)
         self.m_ChangeCharName.setZValue(8)
         self.m_ChangeCharName.setParentItem(self)
-        self.m_ChangeCharName.setPos(QPointF(self.m_BlueChartWidget.lb_Title.pos())+QPointF(10, 10))
+        tt = self.m_BlueChartWidget.lb_Title.pos() + QPointF(10,10)
+        self.m_ChangeCharName.setPos(tt)
         self.m_ChangeCharName.setTextWidth(self.m_BlueChartWidget.top.width())
         self.SetUnselectedWidget()
+        self.m_BlueChartWidget.btn_ShowProperty.setCursor(Qt.PointingHandCursor)
+        self.m_BlueChartWidget.btn_Source.hide()
         self.setWidget(self.m_BlueChartWidgetParent)
+        self.setAcceptHoverEvents(True)
+        self.setAcceptedMouseButtons(Qt.LeftButton)
+        self.setFlags(
+            QGraphicsItem.ItemIsSelectable |
+            QGraphicsItem.ItemIsMovable |
+            QGraphicsItem.ItemSendsGeometryChanges
+        )
         self.setZValue(4)
+
+    def InitSingle(self):
+        self.m_BlueChartWidget.btn_ShowProperty.clicked.connect(self.ShowProperty)
 
     def SetUnselectedWidget(self):
         self.m_BlueChartWidgetParent.SetStyle("Widget")
@@ -62,24 +78,6 @@ class CBlueChartUI(QGraphicsProxyWidget):
         x = pos.x() + ePos.x() - sPos.x()
         y = pos.y() + ePos.y() - sPos.y()
         self.setPos(x, y)
-
-
-class CBlueChartWidget(Ui_BlueChartWidget, slotui.CWidget):
-    def __init__(self, action, parent=None):
-        super(CBlueChartWidget, self).__init__(parent)
-        self.setupUi(self)
-        self.m_Name = action.GetName()
-        self.Init()
-        self.InitSignal()
-        self.show()
-
-    def Init(self):
-        self.lb_Title.setText(self.m_Name)
-        self.btn_ShowProperty.setCursor(Qt.PointingHandCursor)
-        self.btn_Source.hide()
-
-    def InitSignal(self):
-        self.btn_ShowProperty.clicked.connect(self.ShowProperty)
 
     def ShowProperty(self):
         pass
