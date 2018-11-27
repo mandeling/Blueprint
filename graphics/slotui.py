16d14b19c3beaf0bffb44100ef886e04e246a684
lamborghini1993 <1323242382@qq.com>
2018-11-27 10:51:06 +0800

    删除多余

--- graphics/slotui.py
+++ graphics/slotui.py
@@ -15,35 +15,6 @@ from .slotmgr import GetSlotMgr
 from . import define
 
 
-class CWidget(QWidget):
-    def __init__(self, parent=None):
-        super(CWidget, self).__init__(parent)
-        self.m_Stype = {}
-        self.setCursor(Qt.SizeAllCursor)
-
-    def mousePressEvent(self, event):
-        event.accept()
-
-    def GetStyle(self):
-        style = self.styleSheet()
-        self.m_Stype["Widget"] = ""
-        self.m_Stype["Press"] = ""
-        sWidgetStyle = "QWidget#outline{background:transparent;}"
-        sWidgetPressStr = "QWidget#outline{"
-        sWidgetPressStyle = ""
-        iIndex = style.find(sWidgetPressStr)
-        if iIndex != -1:
-            tmpStyle = style[iIndex:]
-            iEnd = tmpStyle.find("}") + 1
-            sWidgetPressStyle = tmpStyle[:iEnd]
-            style = style.replace(sWidgetPressStyle, "")
-        self.m_Stype["Widget"] = style + sWidgetStyle
-        self.m_Stype["Press"] = style + sWidgetPressStyle
-
-    def SetStyle(self, state):
-        self.setStyleSheet(self.m_Stype.get(state, ""))
-
-
 class CSlotUI(QGraphicsPolygonItem):
     def __init__(self, uid, oSlot, parent=None):
         super(CSlotUI, self).__init__(parent)
