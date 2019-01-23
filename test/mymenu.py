1859fe1e47dedf4b7b6c84ee545b1674a104b320
lamborghini-ubuntu <1323242382@qq.com>
2019-01-23 14:44:41 +0800

    fix menu question

--- test/mymenu.py
+++ test/mymenu.py
@@ -2,22 +2,32 @@
 """
 @Author: lamborghini
 @Date: 2019-01-21 15:48:39
-@Desc: 右键自定义菜单，里面添加lineedit无法输入中文问题
+@Desc: 
+1. 右键自定义菜单，里面添加lineedit无法输入中文问题 2019-01-21
+2. 修复了之后ubuntu下运行lineedit无法输入，window正常
 """
 import sys
 
 from PyQt5.QtWidgets import QMenu, QWidget, QHBoxLayout, QLineEdit, QApplication
+from PyQt5.QtCore import Qt, QSize
 
 
 class CMainWindow(QWidget):
     def __init__(self, parent=None):
         super(CMainWindow, self).__init__(parent)
+        self.InitUI()
 
-    def contextMenuEvent(self, event):
-        menu = CMenu()
-        pos = self.mapToGlobal(event.pos())
-        menu.move(pos.x(), pos.y())
-        menu.exec_()
+    def InitUI(self):
+        # self.setWindowFlags(Qt.Popup | Qt.Sheet)
+        hbox = QHBoxLayout(self)
+        line = QLineEdit(self)
+        hbox.addWidget(line)
+
+    # def contextMenuEvent(self, event):
+    #     self.m_Menu = CMenu()
+    #     pos = self.mapToGlobal(event.pos())
+    #     self.m_Menu.move(pos.x(), pos.y())
+    #     self.m_Menu.show()
 
 
 class CMenu(QMenu):
@@ -26,9 +36,13 @@ class CMenu(QMenu):
         self.InitUI()
 
     def InitUI(self):
+        self.setWindowFlags(Qt.Popup | Qt.Sheet)
         hbox = QHBoxLayout(self)
         line = QLineEdit(self)
         hbox.addWidget(line)
+
+    def sizeHint(self):
+        return QSize(200, 50)
 
 
 if __name__ == "__main__":
